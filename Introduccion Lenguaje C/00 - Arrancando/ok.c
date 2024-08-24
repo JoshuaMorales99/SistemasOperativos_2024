477508a48b911bae1f3eb9803f47d3c4c6a5bd8a
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2024-08-24 21:17:02 +0000

    Anido Introduccion Lenguaje C / 00 - Arrancando

---
+++ Introduccion Lenguaje C/00 - Arrancando/ok.c
@@ -0,0 +1,14 @@
+/* --------------------------------------------------------------------------------------------
+ - ENUNCIADO:
+-----------------------------------------------------------------------------------------------
+    Crear el programa mas chico de C.
+
+    NOTA: Para mostrar lo que retorna el programa, procedemos de la siguiente manera. En consola:
+        $ gcc ok.c -o ok                (Para compilar el programa 'ok.c' y generar el ejecutable 'ok')
+        $ ./ok                          (Para ejecutar el ejecutable 'ok')
+        $ echo $?                       (Para mostrar el valor de retorno del programa)
+-------------------------------------------------------------------------------------------- */
+
+int main(void) {
+    return 0;
+}
