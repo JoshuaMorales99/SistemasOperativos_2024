06cf7eee77e2962019a2d2186955a8358fe371ce
JoshuaMorales99 <jmorales@frba.utn.edu.ar>
2024-09-02 03:20:30 +0000

    Modularizo Sockets / Cliente

--- Sockets/Cliente/cliente.c
+++ Sockets/Cliente/cliente.c
@@ -13,63 +13,29 @@
 #include <errno.h>
 #include <commons/error.h>
 
+// Definiciones de conexion.
+#define IP "127.0.0.1"
+#define PUERTO "4444"
+
+// Iniciar modulo cliente.
+int iniciarCliente();
+// Verificar si ocurrio un error y reportarlo.
+void verificarError(int err, int *fd_conexion, const char *mensaje);
+// Obtener informacion de la red.
+void obtenerInfoRed(const struct addrinfo *hints, struct addrinfo **server_info);
+// Crear socket del cliente.
+int crearSocket(const struct addrinfo *server_info);
+// Conectar con el socket del servidor.
+void conectarSocket(const struct addrinfo *server_info, int *fd_conexion);
+
+// -------------------------------------------------------------------------------------------
+
 int main() {
-    // ---------------------------------- SOCKET ----------------------------------
-    // Þara guardar el codigo de error (Si lo hubiera)
-    int err;
-    // Puntero que apunta hacia los datos necesarios para la creación del socket.
-    struct addrinfo hints, *server_info;
+    // Iniciar cliente.
+    int fd_conexion = iniciarCliente();
 
-    // Establecer el hints con 0s.
-    memset(&hints, 0, sizeof(hints));
-    // Especifica la familia de direcciones deseada para las direcciones devueltas.
-    hints.ai_family = AF_INET;
-    // Especifica el tipo de socket preferido.
-    hints.ai_socktype = SOCK_STREAM;
-
-    // Devolver informacion de red sobre la IP y puerto.
-    err = getaddrinfo("127.0.0.1", "4444", &hints, &server_info);
-
-    // Verificar si hubo error al obtener informacion de red (Si lo hubo, err sera distinto a 0)
-    if(err) {
-        // Imprimir por pantalla el mensaje de error.
-        error_show("Error al obtener informacion de red - Socket cliente");
-        // Abortar el programa.
-        abort();
-    }
-
-    // Crear el socket del cliente.
-    int fd_conexion = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
-    // Verificar si hubo error al crear socket de conexion del cliente.
-    if(fd_conexion == -1) {
-        // Imprimir por pantalla el mensaje de error.
-        error_show("Error al crear socket de conexion - Socket cliente");
-        // Cerrar el socket de conexion del cliente.
-        close(fd_conexion);
-        // Abortar el programa.
-        abort();
-    }
-
-    // ---------------------------------- CONNECT ----------------------------------
-    // Conectar el cliente con el socket servidor.
-    err = connect(fd_conexion, server_info->ai_addr, server_info->ai_addrlen);
-    // Verificar si hubo error al conectar el cliente con el socket servidor.
-    if(err == -1) {
-        // Imprimir por pantalla el mensaje de error.
-        error_show("Error al conectar el cliente con el socket servidor - Socket cliente");
-        // Cerrar el socket de conexion del cliente.
-        close(fd_conexion);
-        // Abortar el programa.
-        abort();
-    }
-
-    // Liberar la memoria del server_info.
-    freeaddrinfo(server_info);
-
-    // ---------------------------------- SEND ----------------------------------
     // Cantidad de bytes enviados y/o recibidos.
     size_t bytes;
-
     // Entero a enviar.
     int32_t handshake = 1;
     // Para guardar la respuesta del servidor.
@@ -78,30 +44,15 @@ int main() {
     // Enviar un entero al servidor.
     bytes = send(fd_conexion, &handshake, sizeof(int32_t), 0);
     // Verificar si hubo error al enviar un entero al servidor.
-    if(bytes == -1) {
-        // Imprimir por pantalla el mensaje de error.
-        error_show("Error al enviar un entero al servidor - Socket cliente");
-        // Cerrar el socket de conexion del cliente.
-        close(fd_conexion);
-        // Abortar el programa.
-        abort();
-    }
+    verificarError(bytes, &fd_conexion, "Error al enviar un entero al servidor - Socket cliente");
 
     // Imprimir mensaje de estado enviado.
     printf("Entero enviado correctamente\n");
 
-    // ---------------------------------- RECV ----------------------------------
     // Recibir la respuesta del servidor.
     bytes = recv(fd_conexion, &result, sizeof(int32_t), MSG_WAITALL);
     // Verificar si hubo error al recibir respuesta del servidor.
-    if(bytes == -1) {
-        // Imprimir por pantalla el mensaje de error.
-        error_show("Error al recibir la respuesta del servidor - Socket cliente");
-        // Cerrar el socket de conexion del cliente.
-        close(fd_conexion);
-        // Abortar el programa.
-        abort();
-    }
+    verificarError(bytes, &fd_conexion, "Error al recibir la respuesta del servidor - Socket cliente");
 
     // Imprimir mensaje de estado recibido.
     printf("Se recibio correctamente respuesta del servidor\n");
@@ -119,4 +70,67 @@ int main() {
     close(fd_conexion);
 
     return 0;
+}
+
+int iniciarCliente() {
+    // Puntero que apunta hacia los datos necesarios para la creación del socket.
+    struct addrinfo hints, *server_info = NULL;
+
+    // Inicializar hints
+    memset(&hints, 0, sizeof(hints));
+    // Usar IPv4.
+    hints.ai_family = AF_INET;
+    // Usar TCP.
+    hints.ai_socktype = SOCK_STREAM;
+
+    // Obtener informacion de la red.
+    obtenerInfoRed(&hints, &server_info);
+    // Crear el socket del cliente.
+    int fd_conexion = crearSocket(server_info);
+    // Conectar el cliente con el socket servidor.
+    conectarSocket(server_info, &fd_conexion);
+
+    // Liberar la memoria del server_info.
+    freeaddrinfo(server_info);
+
+    return fd_conexion;
+}
+
+void verificarError(int err, int *fd_conexion, const char *mensaje) {
+    // Verificar si ocurrio un error.
+    if(err < 0) {
+        // Imprimir por pantalla el mensaje de error correspondiente.
+        error_show("%s", mensaje);
+        // Cerrar socket de conexion del cliente.
+        if (fd_conexion != NULL) close(*fd_conexion);
+        // Abortar programa.
+        abort();
+    }
+}
+
+void obtenerInfoRed(const struct addrinfo *hints, struct addrinfo **server_info) {
+    // Devolver informacion de red sobre la IP y puerto.
+    int info = getaddrinfo(IP, PUERTO, hints, server_info);
+    // Verificar si hubo error al obtener informacion de red (Si lo hubo, err sera distinto a 0)
+    verificarError(info * (-1), NULL, "Error al obtener informacion de red - Socket cliente");
+}
+
+int crearSocket(const struct addrinfo *server_info) {
+    // Crear el socket del cliente.
+    int fd_conexion = socket(server_info->ai_family, server_info->ai_socktype, server_info->ai_protocol);
+    // Verificar si hubo error al crear socket de conexion del cliente.
+    verificarError(fd_conexion, &fd_conexion, "Error al crear socket de conexion - Socket cliente");
+
+    // Retornar la conexion.
+    return fd_conexion;
+}
+
+void conectarSocket(const struct addrinfo *server_info, int *fd_conexion) {
+    // Conectar el cliente con el socket servidor.
+    int err = connect(*fd_conexion, server_info->ai_addr, server_info->ai_addrlen);
+    // Verificar si hubo error al conectar el cliente con el socket servidor.
+    verificarError(err, fd_conexion, "Error al conectar el cliente con el socket servidor - Socket cliente");
+
+    // Imprimir mensaje de estado conectado.
+    printf("Se ha conectado correctamente con el servidor\n");
 }
